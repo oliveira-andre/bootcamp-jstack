b29099bbbd410754051c3528482156f4e6cfc890
Andre Oliveira <andreoliveirar2d2@gmail.com>
2024-12-29 14:04:36 -0300

    feat: including button, dropdown and input components from shadcdn

--- vite-class/src/views/pages/CreateUser/index.tsx
+++ vite-class/src/views/pages/CreateUser/index.tsx
@@ -1,5 +1,11 @@
+import { Button } from '@views/components/ui/button';
+import { Input } from '@views/components/ui/input';
+
 export function CreateUser() {
   return (
-    <div>CreateUser</div>
+    <div>
+      <Input placeholder="submit" />
+      <Button>Submit</Button>
+    </div>
   );
 }
